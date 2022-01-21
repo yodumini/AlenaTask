0e02a574f767ae783ce1b99747a7c070ee6ad053
sherwin lee <sherwin@pokermaster.com>
2022-01-21 22:07:54 +0800

    Laravel

---
+++ Infyom_Task/resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="{{ route('categories.index') }}"
+       class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
+        <p>Categories</p>
+    </a>
+</li>
+
+
